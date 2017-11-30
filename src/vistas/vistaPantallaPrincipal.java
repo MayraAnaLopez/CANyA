f11d8c7d8db2fbc4dc858df4f97c3cb166aa342b
Punto Digital <Punto Digital@192.168.1.22>
2017-11-30 16:57:38 -0300

    cambios en vista pantalla ajuste.

--- src/vistas/vistaPantallaPrincipal.java
+++ src/vistas/vistaPantallaPrincipal.java
@@ -40,6 +40,11 @@ public class vistaPantallaPrincipal extends javax.swing.JFrame {
 
         btnajustes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/iconConfiguración.png"))); // NOI18N
         btnajustes.setText("Ajustes");
+        btnajustes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnajustesActionPerformed(evt);
+            }
+        });
         jPanel1.add(btnajustes);
 
         btnalumno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/iconEstudiante.png"))); // NOI18N
@@ -82,40 +87,11 @@ public class vistaPantallaPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(vistaPantallaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(vistaPantallaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(vistaPantallaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(vistaPantallaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void btnajustesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnajustesActionPerformed
+       vistaPantallaAjuste ajuste = new vistaPantallaAjuste();
+       ajuste.setVisible(true);
+    }//GEN-LAST:event_btnajustesActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new vistaPantallaPrincipal().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnajustes;
